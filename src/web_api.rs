c82833a74bc8cc9d0d092cc7d8a7bede680743d1
Daniel Cassidy <mail@danielcassidy.me.uk>
2024-11-03 03:52:13 +0000

    Web API: Replace text Hello World response with JSON.

--- src/web_api.rs
+++ src/web_api.rs
@@ -2,14 +2,31 @@ use defmt::{info, unwrap, warn};
 use embassy_executor::Spawner;
 use embassy_net::tcp::TcpSocket;
 use embassy_net::Stack;
+use low_profile::heapless::Json;
 use low_profile::{Router, Service};
+use serde::Serialize;
+
+#[derive(Serialize)]
+struct HelloWorldResponse {
+    message: HelloWorld,
+}
+
+#[derive(Serialize)]
+enum HelloWorld {
+    HelloWorld,
+}
 
 #[embassy_executor::task]
 async fn web_api_task(stack: &'static Stack<cyw43::NetDriver<'static>>) {
     let mut rx_buffer = [0; 1024];
     let mut tx_buffer = [0; 1024];
 
-    let router = Router::new().get("/", || async { "Hello, World!" });
+    let router = Router::new().get("/", || async {
+        Json::<HelloWorldResponse>(HelloWorldResponse {
+            message: HelloWorld::HelloWorld,
+        })
+    });
+
     let mut socket = TcpSocket::new(stack, &mut rx_buffer, &mut tx_buffer);
 
     loop {
