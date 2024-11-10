72b5d97321f55b05344db60370d201212564115d
Daniel Cassidy <mail@danielcassidy.me.uk>
2024-11-10 17:57:14 +0000

    INA219: Add missing operating_mode field to Config.

--- src/ina219/config.rs
+++ src/ina219/config.rs
@@ -1,5 +1,6 @@
 use crate::ina219::bus_voltage_range::BusVoltageRange;
 use crate::ina219::measurement_mode::MeasurementMode;
+use crate::ina219::operating_mode::OperatingMode;
 use crate::ina219::shunt_voltage_gain::ShuntVoltageGain;
 use defmt::Format;
 
@@ -9,4 +10,5 @@ pub struct Config {
     shunt_voltage_gain: ShuntVoltageGain,
     bus_voltage_measurement_mode: MeasurementMode,
     shunt_voltage_measurement_mode: MeasurementMode,
+    operating_mode: OperatingMode,
 }
